4136780230b953960f746f4a576a494c88614a53
JuliusDeng <1576554007@qq.com>
2020-10-27 18:08:32 +0800

    default页面开发

--- src/views/Default.vue
+++ src/views/Default.vue
@@ -3,8 +3,20 @@
 		<div class="row">
 			<button class="col btn btn-light rounded border-0">
 				<my-icon id="icon-jurassic_report" color="text-danger" size="48"></my-icon>
+				<h3>About me</h3>
+			</button>
+			<button class="col btn btn-light rounded border-0">
+				<my-icon id="icon-xinxi" color="text-warning" size="45"></my-icon>
+				<h3>Resume</h3>
 			</button>
 		</div>
+		<div class="row">
+			<button class="col btn btn-light rounded border-0">
+				<my-icon id="icon-gongjuxiang" color="text-success" size="45"></my-icon>
+				<h3>Portfolio</h3>
+			</button>
+		</div>
+		
 	</div>
 </template>
 
